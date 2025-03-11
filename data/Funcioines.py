80533f18070c4e533d8e93247fdcee10d2646e13
rociogzm22 <rocioguzmanarroyo22@gmail.com>
2025-03-11 18:59:26 +0100

    Depurado

---
+++ data/Funcioines.py
@@ -0,0 +1,17 @@
+import os
+import pandas as pd
+import csv as csv
+
+cd = os.getcwd()  # obtenemos el path
+# Carga de datos para que funcione desde cualquier sitio
+pacientes = pd.read_csv(f'{cd}/cohorte_pacientes.csv')
+alergias = pd.read_csv(f'{cd}/backend/data-analisis/sample-data/data/Sample_Game_1/Sample_Game_1_RawTrackingData_Home_Team.csv')
+condiciones = pd.read_csv(f'{cd}/backend/data-analisis/sample-data/data/Sample_Game_1/Sample_Game_1_RawEventsData.csv') 
+
+print(pacientes.head())
+print(pacientes.describe())
+print(pacientes.info())
+
+ #Sustituir los generos por numero y provinciar por nº
+
+
