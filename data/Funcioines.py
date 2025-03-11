eb999e14e5c623cb312ff1c09c2a9eb58527189b
rociogzm22 <rocioguzmanarroyo22@gmail.com>
2025-03-11 19:02:08 +0100

    Raices

--- data/Funcioines.py
+++ data/Funcioines.py
@@ -7,6 +7,10 @@ cd = os.getcwd()  # obtenemos el path
 pacientes = pd.read_csv(f'{cd}/cohorte_pacientes.csv')
 alergias = pd.read_csv(f'{cd}/backend/data-analisis/sample-data/data/Sample_Game_1/Sample_Game_1_RawTrackingData_Home_Team.csv')
 condiciones = pd.read_csv(f'{cd}/backend/data-analisis/sample-data/data/Sample_Game_1/Sample_Game_1_RawEventsData.csv') 
+medicacion = pd.read_csv(f'{cd}/cohorte_medicationes.csv')
+encuentros = pd.read_csv(f'{cd}/cohorte_encuentros.csv')
+procedimientos = pd.read_csv(f'{cd}/cohorte_procedimientos.csv')
+
 
 print(pacientes.head())
 print(pacientes.describe())
