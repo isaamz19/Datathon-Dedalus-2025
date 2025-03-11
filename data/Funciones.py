ee09745e01d53e0bd1a883d2802b0e83e02778d9
rociogzm22 <rocioguzmanarroyo22@gmail.com>
2025-03-11 19:05:07 +0100

    Arreglos

--- data/Funciones.py
+++ data/Funciones.py
@@ -5,8 +5,8 @@ import csv as csv
 cd = os.getcwd()  # obtenemos el path
 # Carga de datos para que funcione desde cualquier sitio
 pacientes = pd.read_csv(f'{cd}/cohorte_pacientes.csv')
-alergias = pd.read_csv(f'{cd}/backend/data-analisis/sample-data/data/Sample_Game_1/Sample_Game_1_RawTrackingData_Home_Team.csv')
-condiciones = pd.read_csv(f'{cd}/backend/data-analisis/sample-data/data/Sample_Game_1/Sample_Game_1_RawEventsData.csv') 
+alergias = pd.read_csv(f'{cd}/cohorte_alegias.csv')
+condiciones = pd.read_csv(f'{cd}/cohorte_condiciones.csv') 
 medicacion = pd.read_csv(f'{cd}/cohorte_medicationes.csv')
 encuentros = pd.read_csv(f'{cd}/cohorte_encuentros.csv')
 procedimientos = pd.read_csv(f'{cd}/cohorte_procedimientos.csv')
